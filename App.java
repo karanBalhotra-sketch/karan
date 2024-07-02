b827102b87b7d5339e7e94d731536c9e57f06e3d
karanBalhotra-sketch <karanbalhotra7@gmail.com>
2024-07-02 11:32:17 +1000

    Create App.java

---
+++ App.java
@@ -0,0 +1,12 @@
+// src/main/java/com/example/App.java
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    public static String getGreeting() {
+        return "Hello, World!";
+    }
+}

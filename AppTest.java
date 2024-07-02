2b1631b2ea6a224115ee608c7263dd1a3bc20043
karanBalhotra-sketch <karanbalhotra7@gmail.com>
2024-07-02 11:31:46 +1000

    Create AppTest.java

---
+++ AppTest.java
@@ -0,0 +1,13 @@
+// src/test/java/com/example/AppTest.java
+package com.example;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void testGetGreeting() {
+        assertEquals("Hello, World!", App.getGreeting());
+    }
+}
